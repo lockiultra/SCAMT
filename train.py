ca93c256a265ef651a8dc6cd64736bb4932eea0d
lockiultra <lockiultra@gmail.com>
2023-09-01 16:37:49 +0300

    Modify predict

--- train.py
+++ train.py
@@ -7,9 +7,11 @@ def train(path_to_csv):
     data = pd.read_csv(path_to_csv)
     disease_pipeline = DiseasePipeline(data)
     disease_pipeline.train()
-    for model in disease_pipeline.models:
-        with open(f'./Models/{model}', 'wb') as f:
-            pickle.dump(disease_pipeline.models[model], f)
+    # for model in disease_pipeline.models:
+    #     with open(f'./Models/{model}', 'wb') as f:
+    #         pickle.dump(disease_pipeline.models[model], f)
+    with open('./Models/disease_pipeline', 'wb') as f:
+        pickle.dump(disease_pipeline, f)
 
 if __name__ == '__main__':
     train(sys.argv[1])
