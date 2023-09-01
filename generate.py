e4fcdf82e782bcc1f5ee0681535a083ea345a4bf
lockiultra <lockiultra@gmail.com>
2023-09-01 19:01:29 +0300

    Add a condition to ensure that only valid molecules are generated

--- generate.py
+++ generate.py
@@ -1,9 +1,17 @@
 from dgllife.model import load_pretrained
+import rdkit.Chem as Chem
 
 def generate():
     model = load_pretrained('DGMG_ZINC_canonical')
     return model(rdkit_mol=True)
 
+def get_valid_smiles():
+    mol = None
+    while mol is None:
+        smiles = generate()
+        mol = Chem.MolFromSmiles(smiles)
+    return smiles
+
 if __name__ == '__main__':
-    smiles_generate = generate()
+    smiles_generate = get_valid_smiles()
     print(smiles_generate)
