68f7878dc91860c65ea0b6cf8740742d6d53abc2
lockiultra <lockiultra@gmail.com>
2023-09-01 14:20:25 +0300

    Add train.py

---
+++ train.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import pickle
+import sys
+from DiseasePipeline import DiseasePipeline
+
+def train(path_to_csv):
+    data = pd.read_csv(path_to_csv)
+    disease_pipeline = DiseasePipeline(data)
+    disease_pipeline.train()
+    for model in disease_pipeline.models:
+        with open(f'./Models/{model}', 'wb') as f:
+            pickle.dump(disease_pipeline.models[model], f)
+
+if __name__ == '__main__':
+    train(sys.argv[1])
